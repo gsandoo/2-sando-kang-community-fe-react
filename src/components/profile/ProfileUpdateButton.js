475f88f8f5fbed757f342ba03806f6c3f08d2d88
sakang <rogersddt@naver.com>
2025-01-08 10:29:01 +0900

    Fix: 닉네임 수정 예외 처리

--- src/components/profile/ProfileUpdateButton.js
+++ src/components/profile/ProfileUpdateButton.js
@@ -14,40 +14,43 @@ const ProfileUpdateButton = ({ nickname, setError, error, file }) => {
     try {
 
       formData.append('user_id', userId);
-      formData.append('nickname', nickname);
       
-      if(file)formData.append('profile', file);
+      if(nickname.length <= 0) alert('닉네임을 입력해주세요');
+      else{
+        formData.append('nickname', nickname);
+      
+        if(file)formData.append('profile', file);
+      
+        const response = await fetch('/api/auth/nickname', {
+          method: 'PATCH',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          body: formData,
+        });
+        const data = await response.json();
     
-      const response = await fetch('/api/auth/nickname', {
-        method: 'PATCH',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        body: formData,
-      });
-      const data = await response.json();
-  
-      if (data.success) {
-        console.log(data.data)
-        alert(`프로필 수정이 완료되었습니다`);
-        setToastVisible(true);
-        setTimeout(() => setToastVisible(false), 2000);
-        saveLocalStorage('nickname', nickname);
-  
-        if(file){
-        
-          const reader = new FileReader();
-          reader.onload = (e) => {
-            const base64Data = e.target.result;
-            console.log(base64Data)
-            saveLocalStorage('profile', base64Data); 
+        if (data.success) {
+          console.log(data.data)
+          alert(`프로필 수정이 완료되었습니다`);
+          setToastVisible(true);
+          setTimeout(() => setToastVisible(false), 2000);
+          saveLocalStorage('nickname', nickname);
+    
+          if(file){
+            const reader = new FileReader();
+            reader.onload = (e) => {
+              const base64Data = e.target.result;
+              console.log(base64Data)
+              saveLocalStorage('profile', base64Data); 
+            }
+            reader.readAsDataURL(file)
           }
-          reader.readAsDataURL(file)
+          handleLocation('/Posts');
+        } else {
+          alert(`${data.message.code}`);
+          setError(data.message.code || '닉네임 수정 실패');
         }
-        handleLocation('/Posts');
-      } else {
-        alert(`${data.message.code}`);
-        setError(data.message.code || '닉네임 수정 실패');
       }
     } catch (error) {
       alert(error);
